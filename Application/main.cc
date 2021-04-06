a88fc20d6eb6b08a7fe5c91747ece641ea7c862f
WayenVan <wayenvan@outlook.com>
2021-04-06 19:14:30 +0100

    finish servo driver

--- Application/main.cc
+++ Application/main.cc
@@ -10,26 +10,5 @@ using namespace std;
 using namespace wayenvan;
 
 int main (){
-    typedef unique_ptr<Detector> Detector_ptr;
-    typedef unique_ptr<ServerVideo> ServerVideo_ptr;
-
-    try{
-        myUtils::share_print("hello");
-        Detector_ptr detector =Detector_ptr(new Detector());
-        ServerVideo_ptr server = ServerVideo_ptr(new ServerVideo("0.0.0.0", 12345));
-
-        detector->registerCamera(Camera::getInstance());
-        server->registerDetector(detector.get());
-
-        detector->start();
-        server->start();
-
-        detector->join();
-        server->join();
-    }catch(const ModuleException& e){
-        myUtils::share_cerr("Error ocurr in "+string(e.get_error_module_name())+":");
-        myUtils::share_cerr(e.what());
-    }
-
-    myUtils::share_print("finished!");
+    cout<<"hello"<<endl;
 }
