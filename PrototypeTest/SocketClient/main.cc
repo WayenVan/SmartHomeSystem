6d782fb9a458c8beef98cc70c385f114a538de7f
Wang Jingyan <wayenvan@outlook.com>
2021-04-19 10:15:14 +0100

    modify c++ client

--- PrototypeTest/SocketClient/main.cc
+++ PrototypeTest/SocketClient/main.cc
@@ -25,7 +25,7 @@ int main(int argc, char** argv){
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+    inet_pton(AF_INET, "192.168.0.104", &server_address.sin_addr);
     
     //connect
     int server_socket = connect(skt, (sockaddr*)&server_address, sizeof(sockaddr_in));
@@ -35,7 +35,7 @@ int main(int argc, char** argv){
         return 0;
     }
     
-    const char buf_send[4] = "get";
+    const int buf_send = 0x11;
     int img_size;
     unsigned char buf_img[128*1024];
     std::vector<uchar> buf_img_vec;
@@ -48,15 +48,18 @@ int main(int argc, char** argv){
         //clear bufffer
         memset(&img_size, 0, sizeof(int));
         memset(buf_img, 0, 128*1024);
+        
 
-        send(skt, buf_send, 3, 0);
+        send(skt, &buf_send, sizeof(buf_send), 0);
 
         usleep(1000*60);
-        recv(skt, &img_size, sizeof(int), 0);
+        recv(skt, &img_size, sizeof(int), MSG_WAITALL);
         std::cout<<"frame size:"<<img_size<<std::endl;
-        if(img_size>0){
-            recv(skt, &buf_img, img_size, 0);
+        if(img_size > 0){
+           
+            int result = recv(skt, &buf_img, img_size, MSG_WAITALL);
 
+            std::cout<<"frame actual size:"<<result<<std::endl;
             // Display the resulting frame
             buf_img_vec.assign(buf_img, buf_img+img_size);
             cv::Mat frame = cv::imdecode(cv::Mat(buf_img_vec), -1);
